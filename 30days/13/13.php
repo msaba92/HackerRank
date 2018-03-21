b89c263032065a686dff3004fe5e32bfff64aa38
Mattia Saba <mattiasaba47734@gmail.com>
2018-03-21 14:26:43 +0100

    30Days Challenge: Day 13

---
+++ 30days/13/13.php
@@ -0,0 +1,34 @@
+<?php
+abstract class Book
+{
+    protected $title;
+    protected  $author;
+    function __construct($t,$a){
+        $this->title=$t;
+        $this->author=$a;
+    }
+    abstract protected function display();
+}
+
+class MyBook extends Book {
+    protected $price;
+    function __construct($title, $author, $price) {
+        parent::__construct($title, $author);
+        $this->price = $price;
+    }
+
+    function display() {
+        echo "Title: " . $this->title;
+        echo "Author: " . $this->author;
+        echo "Price: " . $this->price;
+    }
+}
+
+
+$title=fgets(STDIN);
+$author=fgets(STDIN);
+$price=intval(fgets(STDIN));
+$novel=new MyBook($title,$author,$price);
+$novel->display();
+
+?>
---
+++ 30days/13/13.rb
@@ -0,0 +1,5 @@
+# Ruby does not have true abstract classes, so as far as I know
+# I had a choice between simply skipping this, or implementing
+# some way to trick the input from HackerRank. I opted for the
+# first option, as it wouldn't have made much sense to do the
+# latter.
