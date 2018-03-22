bbeb0a92c84082375838d668c0ac015379e61385
Mattia Saba <mattiasaba47734@gmail.com>
2018-03-22 21:06:16 +0100

    30Days Challenge: Day 17

---
+++ 30days/17/17.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+class RangeError < StandardError
+end
+
+class Calculator
+    def power(n, p)
+        if n < 0 || p < 0
+            raise(RangeError, "n and p should be non-negative")
+        else
+            n**p
+        end
+    end
+end
+
+myCalculator = Calculator.new()
+
+t = gets.to_i
+
+for i in 1..t
+    n, p = gets.split.map(&:to_i)
+    begin
+        ans = myCalculator.power(n, p)
+        puts ans.to_i
+    rescue RangeError => e
+        puts e.message
+    end
+end
